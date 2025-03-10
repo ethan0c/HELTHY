428bdd134e49d645aa829bebba0b8614142c904e
ethan <conyss@gmail.com>
2025-03-10 09:27:58 -0400

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+        const name = 'Ethan';
+        if (name === 'Ethan') {
+            return <h1>Hello {name}</h1>;
+        }
+        return <h1>Hello World</h1>;
+}
+
+export default Message;
